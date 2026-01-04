874687ee07f354f56983dff8f65a20c675029d6b
Nyasha Praise Mapetere <mapeterenyasha@gmail.com>
2026-01-04 15:48:47 +0200

    method signature

---
+++ 06-methods/Greetings.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Greetings {
+
+    public static void main(String[] args) {
+        greetUser();
+    }
+
+    public static void greetUser() {
+        System.out.println("Enter your name: ");
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.next();
+        System.out.println("Hello, " + name);
+        scanner.close();
+    }
+
+}
