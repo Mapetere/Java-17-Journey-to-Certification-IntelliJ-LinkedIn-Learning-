2423ce32dc13f65eb61c2fd953d8a0cefb2cbe37
Nyasha Praise Mapetere <mapeterenyasha@gmail.com>
2026-01-05 15:23:11 +0200

    problem solution:Greeting

--- 06-methods/Greetings.java
+++ 06-methods/Greetings.java
@@ -15,3 +15,34 @@ public class Greetings {
     }
 
 }
+
+// variable scope
+/*
+A variable is limited to the block, statement, or expression in which it is used.
+Method variables can have same name as variables in other methods.  
+for example:
+
+public void method1(){
+String myVariable = "123"
+}
+
+public void method2(){
+String myVariable = "abc"
+}
+
+myVariable is a local variable to each methof hence they cant be globally accessed.
+
+to reference a global variable we use the keyword 'this'
+
+public void method1(){
+String myVariable = "123"
+}
+
+public void method2(){
+String myVariable = "abc"
+}
+
+this.myVariable
+
+*/
+ 
